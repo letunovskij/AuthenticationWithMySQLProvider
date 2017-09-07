b3c739c82ffdb421ce07f05521c77a2433721272
letunovskij <letunovskij.oleg@gmail.com>
2017-09-07 21:34:07 +0300

    initial commit

---
+++ AuthenticationWithMySQLProvider/Models/IdentityModels.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using AspNet.Identity.MySQL;
+
+namespace AuthenticationWithMySQLProvider.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Здесь добавьте утверждения пользователя
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : MySQLDatabase
+    {
+        public ApplicationDbContext(string connectionName)
+            : base(connectionName)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext("DefaultConnection");
+        }
+    }
+}
